f7bb4f2ab9e3f884a99718b618211a97e9d21f28
sterns <stevenernst131@gmail.com>
2016-11-08 10:52:15 -0800

    adding current changes

--- AppLensV2/app/Sia/SiaCtrl.ts
+++ AppLensV2/app/Sia/SiaCtrl.ts
@@ -45,7 +45,8 @@ module SupportCenter {
             }
 
             for (let detectorAnalysisData of this.SiaResponse.Payload) {
-                //console.log(new Date().getTime() + ": Starting for " + detectorAnalysisData.Source);
+                var timer = new Date().getTime();
+               // console.log( + ": Starting for " + detectorAnalysisData.Source);
                 this.DetectorData[detectorAnalysisData.Source] = new DetectorViewParams();
                 this.DetectorData[detectorAnalysisData.Source].loading = false;
                 this.DetectorData[detectorAnalysisData.Source].chartoptions = detectorViewHelper.GetChartOptions(detectorAnalysisData.Source);
@@ -70,7 +71,7 @@ module SupportCenter {
                 this.DetectorsService.getDetectorSolution(detectorAnalysisData.Source).then(function (solutionResponse) {
                     self.DetectorData[detectorAnalysisData.Source].solution = solutionResponse;
                 });
-                //console.log(new Date().getTime() + ": Ending for " + detectorAnalysisData.Source);
+                console.log(detectorAnalysisData.Source + ": " + (new Date().getTime() - timer));
             }
         }
 
