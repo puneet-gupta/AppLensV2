37e006554d03612c7a30e710f13a2577a2de5053
sterns <stevenernst131@gmail.com>
2016-11-08 09:19:37 -0800

    Removing tracing

--- AppLensV2/app/Sia/SiaCtrl.ts
+++ AppLensV2/app/Sia/SiaCtrl.ts
@@ -45,7 +45,7 @@ module SupportCenter {
             }
 
             for (let detectorAnalysisData of this.SiaResponse.Payload) {
-                console.log(new Date().getTime() + ": Starting for " + detectorAnalysisData.Source);
+                //console.log(new Date().getTime() + ": Starting for " + detectorAnalysisData.Source);
                 this.DetectorData[detectorAnalysisData.Source] = new DetectorViewParams();
                 this.DetectorData[detectorAnalysisData.Source].loading = false;
                 this.DetectorData[detectorAnalysisData.Source].chartoptions = detectorViewHelper.GetChartOptions(detectorAnalysisData.Source);
@@ -70,7 +70,7 @@ module SupportCenter {
                 this.DetectorsService.getDetectorSolution(detectorAnalysisData.Source).then(function (solutionResponse) {
                     self.DetectorData[detectorAnalysisData.Source].solution = solutionResponse;
                 });
-                console.log(new Date().getTime() + ": Ending for " + detectorAnalysisData.Source);
+                //console.log(new Date().getTime() + ": Ending for " + detectorAnalysisData.Source);
             }
         }
 
